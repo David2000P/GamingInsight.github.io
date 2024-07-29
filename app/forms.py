4bdecbb6c9e5078981b6b87a645414254109da09
David Pätzold <167893375+David2000P@users.noreply.github.com>
2024-07-29 16:46:37 +0200

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -27,16 +27,28 @@ class LoginForm(FlaskForm):
     submit = SubmitField('Sign In')
 
 class CompanyRegistrationForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    company_name = StringField('Company Name', validators=[DataRequired()])
+    company_description = TextAreaField('Company Description', validators=[DataRequired()])
+    company_website = StringField('Company Website', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
-    company_name = StringField('Company Name', validators=[DataRequired()])
-    company_description = TextAreaField('Company Description', validators=[DataRequired()])
-    company_website = StringField('Company Website', validators=[DataRequired(), URL()])
     submit = SubmitField('Register')
+
+class CompanyLoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign In')
+
 
     def validate_company_name(self, company_name):
         company = Company.query.filter_by(name=company_name.data).first()
         if company is not None:
             raise ValidationError('Please use a different company name.')
+
+class CompanyEditForm(FlaskForm):
+    company_name = StringField('Company Name', validators=[DataRequired()])
+    company_description = TextAreaField('Company Description', validators=[DataRequired()])
+    company_website = StringField('Company Website', validators=[DataRequired(), URL()])
+    submit = SubmitField('Update')
