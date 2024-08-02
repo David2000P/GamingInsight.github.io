e4015861d33afa8034fa19ee52ea7ad8a4bddfcd
ektotheo <138446648+ektotheo@users.noreply.github.com>
2024-08-02 15:24:29 +0200

    Update forms.py

--- app/forms.py
+++ app/forms.py
@@ -1,75 +1,74 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField, SelectField
-from wtforms.validators import DataRequired, Email, EqualTo, URL, ValidationError
-from .models import User
-
-class RegistrationForm(FlaskForm):
-    username = StringField('Benutzername', validators=[DataRequired()])
-    email = StringField('E-Mail', validators=[DataRequired(), Email()])
-    password = PasswordField('Passwort', validators=[DataRequired()])
-    password2 = PasswordField('Passwort wiederholen', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Registrieren')
-
-    def validate_username(self, username):
-        user = User.query.filter_by(username=username.data).first()
-        if user is not None:
-            raise ValidationError('Bitte verwenden Sie einen anderen Benutzernamen.')
-
-    def validate_email(self, email):
-        user = User.query.filter_by(email=email.data).first()
-        if user is not None:
-            raise ValidationError('Bitte verwenden Sie eine andere E-Mail-Adresse.')
-
-class LoginForm(FlaskForm):
-    username_or_email = StringField('Benutzername oder E-Mail', validators=[DataRequired()])
-    password = PasswordField('Passwort', validators=[DataRequired()])
-    remember_me = BooleanField('Eingeloggt bleiben')
-    submit = SubmitField('Einloggen')
-
-
-class CompanyRegistrationForm(FlaskForm):
-    company_name = StringField('Name des Unternehmens', validators=[DataRequired()])
-    company_description = TextAreaField('Beschreibung', validators=[DataRequired()])
-    company_website = StringField('Webseite', validators=[DataRequired()])
-    submit = SubmitField('Jetzt registrieren')
-
-    def validate_company_website(self, field):
-        if not field.data.startswith(('http://', 'https://')):
-            field.data = 'http://' + field.data
-        try:
-            URL()(None, field)
-        except ValidationError:
-            raise ValidationError('Ungültige URL')
-
-class CompanyLoginForm(FlaskForm):
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
-
-class CompanyEditForm(FlaskForm):
-    company_name = StringField('Company Name', validators=[DataRequired()])
-    company_description = TextAreaField('Company Description', validators=[DataRequired()])
-    company_website = StringField('Company Website', validators=[DataRequired(), URL()])
-    submit = SubmitField('Update')
-
-class ReviewForm(FlaskForm):
-    role = SelectField('Rolle', choices=[
-        ('dev', 'Entwickler'), ('manager', 'Manager'), 
-        ('game_designer', 'Game Designer'), ('level_designer', 'Level Designer'),
-        ('programmer', 'Programmierer'), ('ui_ux_designer', 'UI/UX Designer'),
-        ('artist_animator', 'Artist/Animator'), ('sound_designer', 'Sound Designer'),
-    ], validators=[DataRequired()])
-    category = SelectField('Kategorie', choices=[
-        ('worklife', 'Work-Life-Balance'), ('pay', 'Bezahlung'),
-        ('teamwork', 'Teamarbeit'), ('project_dynamics', 'Projektdynamik'),
-        ('work_environment', 'Arbeitsumgebung'), ('tech_ressources', 'Technologische Ressourcen'),
-        ('creative_freedom', 'Kreative Freiheit')
-    ], validators=[DataRequired()])
-    rating = SelectField('Bewertung', choices=[('1', '1'), ('2', '2'), ('3', '3'), ('4', '4'), ('5', '5')], validators=[DataRequired()])
-    comments = TextAreaField('Kommentare', validators=[DataRequired()])
-    submit = SubmitField('Bewertung absenden')
 <<<<<<< HEAD
-=======
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_mail import Mail
 
+db = SQLAlchemy()
+migrate = Migrate()
+login_manager = LoginManager()
+mail = Mail()
+=======
+from .extensions import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
+from datetime import datetime, timedelta
+import jwt
+from time import time
+from flask import current_app
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    confirmed = db.Column(db.Boolean, default=False)
+    # Reviews geschrieben von Benutzer
+    written_reviews = db.relationship('Review', backref='author', lazy='dynamic')
+    companies = db.relationship('Company', backref='owner', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+    def get_reset_token(self, expires_sec=1800):
+        expires = datetime.utcnow() + timedelta(seconds=expires_sec)
+        token = jwt.encode({'user_id': self.id, 'exp': expires}, current_app.config['SECRET_KEY'], algorithm='HS256')
+        return token
+
+    @staticmethod
+    def verify_reset_token(token):
+        try:
+            decoded_token = jwt.decode(token, current_app.config['SECRET_KEY'], algorithms=['HS256'])
+            user_id = decoded_token.get('user_id')
+        except jwt.ExpiredSignatureError:
+            return None
+        except jwt.InvalidTokenError:
+            return None
+        return User.query.get(user_id)
+
+
+class Company(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+    website = db.Column(db.String(120), nullable=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    # Reviews für das Unternehmen
+    received_reviews = db.relationship('Review', backref='company', lazy='dynamic')
+
+class Review(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    comments = db.Column(db.Text, nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    role = db.Column(db.String(64), nullable=False)
+    category = db.Column(db.String(64), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    company_id = db.Column(db.Integer, db.ForeignKey('company.id'), nullable=False)
+    timestamp = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+
+    user = db.relationship('User', backref='reviews')
 >>>>>>> 420431ae78fdf88e531a23fcdc38dd01a64d03b9
